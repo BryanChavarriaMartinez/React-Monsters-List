addbc8ec9ddc28a7bacd1ecf90d1a7210256fdf3
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-05-24 15:00:28 -0600

    Search Box added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 class App extends Component {
+  // Step 1: Our constructor runs first, our State gets initialized.
   constructor() {
     super();
+
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
+  // Step 3: Here we fetch JSON asynchronous, and update the State 
+  // After we update the State we Re-render the page (Step 2) to update the values.
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((Response) => Response.json())
@@ -24,10 +29,27 @@ class App extends Component {
     ));
   }
 
+  // Step 2: The initial rendering and mounting of our component on to the page.
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='Search Monsters' 
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            })
+          }} 
+        />
+
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
