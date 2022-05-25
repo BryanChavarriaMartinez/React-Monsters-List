827664202c774bcb25246899e094b960af9f9c97
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-05-25 16:31:28 -0600

    Project completed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import './card-list.styles.css';
+import Card from '../card/card.component';
 
 class CardList extends Component {
     render() {
@@ -6,13 +8,10 @@ class CardList extends Component {
         const { monsters } = this.props;
         return ( 
             <div className="card-list">
-            {monsters.map((monster) => (
-                <div className="card-container" key={monster.id}>
-                    <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-                    <h2>{monster.name}</h2>
-                    <p>{monster.email}</p>
-                </div>
-            ))}
+            {monsters.map((monster) => {
+                return (
+                    <Card monster={monster} />
+            )})}
             </div>
         )
     }
