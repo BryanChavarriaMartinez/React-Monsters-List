223194a8f442ee2274a57321489b1c27ac8893f8
Bryan CHAVARRIA <bchavarria@firstronic.com>
2022-05-30 13:39:06 -0600

    Firstr project completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import { Component } from 'react'; //This is our Class component
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
@@ -7,16 +7,40 @@ import './App.css';
 const App = () => {
 
   const [searchField, setSearchField] = useState(''); // [value, setValue]
-  
+  const [title, setTitle] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((Response) => Response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
 
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
+
   return (
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder='Search Monsters' />
+      <br />
+      <SearchBox className='title-search-box' onChangeHandler={onTitleChange} placeholder='Set Title' />
       <CardList monsters={filteredMonsters} />
     </div>
   );
